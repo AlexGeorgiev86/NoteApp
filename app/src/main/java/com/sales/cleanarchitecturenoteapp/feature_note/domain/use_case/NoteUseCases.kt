3daad80f0824d648bb262016273590aee35f0316
aleksandar.kgeorgiev <aleksandar.kgeorgiev@scalefocus.com>
2024-02-29 14:42:34 +0200

    usecases, viewModel

---
+++ app/src/main/java/com/sales/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,8 @@
+package com.sales.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+data class NoteUseCases(
+    val getNotes: GetNotes,
+    val addNote: AddNote,
+    val deleteNote: DeleteNote
+) {
+}
